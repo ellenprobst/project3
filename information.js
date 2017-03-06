481ed3c3db63cf3e95401b1dde77cece94dd8d78
Ellen Probst <ellenprobst@hotmail.com>
2017-03-06 17:19:48 -0500

    first commit

---
+++ information.js
@@ -0,0 +1,33 @@
+var text = [
+	{
+		name: "frame1",
+		paragraph1: "Conflict has displaced me from home", 
+		paragraph2 : "There are 65.3 million of us around the world!",
+		paragraph3: "Every Year Canada receives approximately more than 13,500 refugees"
+	},
+	{
+		name: "frame2",
+		paragraph1: "I was lucky, I was a refugee assisted by the canadian government", 
+		paragraph2 : "Some others traveled here sponsored privately",
+		paragraph3: "The process usually takes 6 months"
+	},
+	{
+		name: "frame3",
+		paragraph1: "I didn't speak a word of English", 
+		paragraph2 : "I was out of school for some months",
+		paragraph3: "And I am not used to the weather"
+	},
+	{
+		name: "frame4",
+		paragraph1: "Canadians have been very helpful!", 
+		paragraph2 : "Thanks to the community, I've found an amazing school",
+		paragraph3: "I have learnt English and I have a pretty cool jacket"
+	},
+	{
+		name: "frame5",
+		paragraph1: "Thanks Canada! your humanitarian action has...", 
+		paragraph2 : "...brought me to a safe place",
+		paragraph3: "Let's keep being humanitarian"
+	}
+]
+	
